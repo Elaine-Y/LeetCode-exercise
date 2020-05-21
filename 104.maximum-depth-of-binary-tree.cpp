44e36137dbd6388049f5d63920d59a260c04593e
Elaine <627533068@qq.com>
2020-05-21 23:44:09 +0800

    easy code finished

---
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=104 lang=cpp
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (64.57%)
+ * Likes:    2266
+ * Dislikes: 69
+ * Total Accepted:    775K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its depth = 3.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+      if (root == NULL) return 0;
+      int left_max = maxDepth(root->left);
+      int right_max = maxDepth(root->right);
+      return 1 + std::max(left_max, right_max);
+    }
+};
+// @lc code=end
+// 树的最大深度=根节点1+max(左右子树的深度)
+// 根节点和其余子节点计算方法是一样的，所以用递归    深度优先搜索的策略dfs
